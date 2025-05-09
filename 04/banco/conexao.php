5bfa10750e3b440cfe244498212152b3f1c39395
standbytheboy <moraislcs05@gmail.com>
2025-05-08 21:59:32 -0300

    integrando frontend, backend e database - 2

--- 04/banco/conexao.php
+++ 04/banco/conexao.php
@@ -7,19 +7,19 @@ $database = "loja_produtos";
 try {
     $db = new PDO("mysql:host=localhost;dbname=$database", $user, "");
 
-    $resultado = $db->query("SELECT * FROM produtos");
-
-    $produtos = $resultado->fetchAll(PDO::FETCH_ASSOC);
     if (isset($_GET['nome']) && isset($_GET['preco'])) {
         $nome = $_GET['nome'];
         $preco = $_GET['preco'];
-
+        
         $stmt = $db->prepare("INSERT INTO produtos (nome, preco) VALUES (:nome, :preco)");
-
+        
         $stmt->execute([
             ":nome"=> $nome,
             ":preco"=> $preco
         ]);
+        $resultado = $db->query("SELECT * FROM produtos");
+        $produtos = $resultado->fetchAll(PDO::FETCH_ASSOC);
+        
         header("Location: conexao.php");
         exit;
     }
