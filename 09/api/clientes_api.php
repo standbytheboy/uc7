32c010ecfa3c89e6a1e64049600075068ef8ae4f
lucas <moraislcs05@gmail.com>
2025-06-02 19:43:10 -0300

    aula 09 - endpoints basicos

--- 09/api/clientes_api.php
+++ 09/api/clientes_api.php
@@ -10,8 +10,20 @@ switch ($action)
     case 'listar':
         echo json_encode($dao->getAll());
         break;
+    case 'buscar':
+        echo json_encode("Chamou o buscar");
+        break;
+    case 'cadastrar':
+        echo json_encode("Chamou o cadastrar");
+        break;
+    case 'atualizar':
+        echo json_encode("Chamou o atualizar");
+        break;
+    case 'excluir':
+        echo json_encode("Chamou o excluir");    
+        break;
     default:
         http_response_code(400);
-        echo json_encode(['error' => 'Acao inválida, informar action']);
+        echo json_encode(['error' => 'Ação inválida, informar action']);
         break;
 }
